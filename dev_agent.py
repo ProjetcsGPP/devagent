d31be860c3330dc9bde3227a0974c6985d3f9863
Alexandre Mohamad <gpp@seger.es.gov.br>
2026-04-27 14:12:13 -0300

    init devagent clean architecture v1

---
+++ dev_agent.py
@@ -0,0 +1,10 @@
+"""
+dev_agent.py
+
+Launcher principal.
+"""
+
+from dev_agent.main import main
+
+if __name__ == "__main__":
+    main()
---
+++ src/devagent/rag/llm_ollama.py
@@ -0,0 +1,12 @@
+import requests
+
+OLLAMA_URL = "http://localhost:11434/api/generate"
+
+def ask_llm(prompt):
+    response = requests.post(OLLAMA_URL, json={
+        "model": "qwen2.5",
+        "prompt": prompt,
+        "stream": False
+    })
+
+    return response.json()["response"]
